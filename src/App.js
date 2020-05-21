420cac161ea8091cb96891f8c95a2167582745cc
unknown <sachinrk0412@gmail.com>
2020-05-21 22:59:49 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tabletop from 'tabletop';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      datas: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1guVXhszJbbETwTd5uvxtO7pcYlDOQon1D-RVyWCRNwc',
+      callback: googleData => {
+        console.log('google sheet data --->', googleData)
+        this.setState({
+          datas: googleData
+        })
+      },
+      simpleSheet: true
+    })
+  }
+
+  // https://docs.google.com/spreadsheets/d/1guVXhszJbbETwTd5uvxtO7pcYlDOQon1D-RVyWCRNwc/edit?usp=sharing
+
+  render() {
+    console.log(this.state.datas)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React + Google Sheets Demo</h1>
+        </header>
+        <div className="usersDetails">
+          {
+            this.state.datas.length > 0 ?
+              this.state.datas.map(obj => {
+                return (
+                  <div key={obj.id}>
+                    <p>{obj.name}</p>
+                    <p>{obj.contactno}</p>
+                    <p>{obj.address}</p>
+                  </div>
+                )
+              })
+            : null
+          }
+        </div>
+      </div>
+    );
+  };
+};
 
 export default App;
